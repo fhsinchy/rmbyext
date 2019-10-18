9eed706c5a47ae32e60288c4acc62f552a5c7d55
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2019-10-19 02:10:51 +0600

    Go script done

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"io/ioutil"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if(len(args) > 0) {
+		p, err := os.Getwd()
+		
+		if(err != nil) {
+			panic(err)
+		}
+
+		var deletedFileCount int
+		var log string
+
+		for _, arg := range args {
+			var files []string = createFileList(p, arg)
+
+			if len(files) > 0 {
+				removeFiles(arg, files, &deletedFileCount, &log)
+			} else {
+				fmt.Printf("NO %v FILES TO REMOVE.\n", strings.ToUpper(arg))
+			}
+		}
+
+		if (deletedFileCount > 0) {
+			writeLogToFile(&log, &deletedFileCount)
+		}
+	}
+}
+
+func createFileList(p string, arg string) []string {
+	var files []string
+
+	filepath.Walk(p, func(path string, file os.FileInfo, _ error) error {
+		if !file.IsDir() {
+			if filepath.Ext(path) == "." + arg {
+				absolutePath, _ := filepath.Abs(path)
+				files = append(files, absolutePath)
+			}
+		}
+		return nil
+	})
+
+	return files
+}
+
+func removeFiles(arg string, files []string, deletedFileCount *int, log *string) {
+	fmt.Printf("REMOVING ALL '%v' FILES -->\n", strings.ToUpper(arg))
+	for _, file := range files {
+		os.Remove(file)
+		*deletedFileCount++
+		fmt.Println(file)
+		*log += file + "\n"
+	}
+	*log += "\n"
+	fmt.Print("\n")
+}
+
+func writeLogToFile(log *string, deletedFileCount *int) {
+	ioutil.WriteFile("delete_log.log", []byte(*log + fmt.Sprintf("%v FILES DELETED.\n", *deletedFileCount)), 0666)
+}
